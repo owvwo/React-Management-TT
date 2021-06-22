e8f0143a7eb37d9fcfe189b9df3e00f05a2f6eff
최승훈 <chhoon97@naver.com>
2021-06-22 14:25:38 +0900

    Add Get Customers REST API MODULE

--- client/src/App.js
+++ client/src/App.js
@@ -18,36 +18,25 @@ const styles = theme => ({
   table: {
     minWidth: 1080
   }
-})
+});
 
-const customers = [
-  {
-  'id': 1,
-  'image': 'https://placeimg.com/64/64/1',
-  'name' : '홍길동',
-  'birthday' : '961222',
-  'gender': '남자',
-  'job' : '대학생'
-  },
-  {
-    'id': 2,
-    'image': 'https://placeimg.com/64/64/2',
-    'name' : '야야야',
-    'birthday' : '921222',
-    'gender': '남자',
-    'job' : '대학생'
-  },
-  {
-    'id': 3,
-    'image': 'https://placeimg.com/64/64/3',
-    'name' : '나야야',
-    'birthday' : '9211222',
-    'gender': '남자',
-    'job' : '대학생'
-  }
-]
 
 class App extends React.Component {
+
+  state = {
+    customers: ""
+  }
+  componentDidMount(){
+    this.callApi()
+      .then(res => this.setState({customers: res})) // body가 return되어서 res로 변환 customers라는 state에 저장\
+      .catch(err => console.log(err));
+  }
+  callApi = async () =>{
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
   render() {
     const {classes} = this.props;
     return (
@@ -64,7 +53,9 @@ class App extends React.Component {
             </TableRow>
           </TableHead>
           <TableBody>
-            {customers.map(c => { return (<Customer key={c.id} id={c.id} image={c.image} birthday={c.birthday} name={c.name} gender={c.gender} job={c.job} />); })}
+            {this.state.customers ? this.state.customers.map(c => { 
+              return (<Customer key={c.id} id={c.id} image={c.image} birthday={c.birthday} name={c.name} gender={c.gender} job={c.job} />); 
+            }) : ""}
           </TableBody>
         </Table>
       </Paper>
--- server.js
+++ server.js
@@ -6,8 +6,37 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true}))
 
-app.get('/api/hello', (req, res) =>{
-    res.send({message: 'Heelo Express!'});
+app.get('/api/customers', (req, res) =>{
+    res.send([
+        {
+            'id': 1,
+            'image': 'https://placeimg.com/64/64/1',
+            'name' : '홍길동',
+            'birthday' : '961222',
+            'gender': '남자',
+            'job' : '대학생'
+            },
+            {
+              'id': 2,
+              'image': 'https://placeimg.com/64/64/2',
+              'name' : '야야야',
+              'birthday' : '921222',
+              'gender': '남자',
+              'job' : '대학생'
+            },
+            {
+              'id': 3,
+              'image': 'https://placeimg.com/64/64/3',
+              'name' : '나야야',
+              'birthday' : '9211222',
+              'gender': '남자',
+              'job' : '대학생'
+            }
+    ])
+});
+
+app.use(function (req,res){
+    res.send(`<p>박정우치팅데이</p>`);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
